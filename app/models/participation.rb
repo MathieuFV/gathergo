00b7f21a27834637c93adde909d82e6e7ba8538c
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-10-30 15:38:55 +0100

    Add validations to participation model

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,4 +3,8 @@ class Participation < ApplicationRecord
   belongs_to :trip
 
   has_many :availabilities, dependent: :destroy
+
+  validates :user_id, presence: true, uniqueness: { scope: :trip_id }
+  validates :trip_id, presence: true
+  validates :role, presence: true, inclusion: { in: ["admin", "participant"] }
 end
--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.describe Participation, type: :model do
   # it "has a valid user id" do
   # end
+  let(:user) {
+    User.create!(
+      first_name: "François",
+      address: "Paris",
+      email: "francois@example.com",
+      password: "password",
+      phone: "3636"
+    )
+  }
+  let(:trip) { Trip.create!(name: "Voyage", start_date: Date.today, end_date: Date.tomorrow) }
+
   it { should belong_to(:user) }
   it { should belong_to(:trip) }
 
@@ -12,20 +23,11 @@ RSpec.describe Participation, type: :model do
   it { should validate_presence_of(:trip_id) }
   it { should validate_presence_of(:role) }
 
-  it { should validate_inclusion_of(:role).in_array(["admin", "paticipant"]) }
+  it { should validate_inclusion_of(:role).in_array(["admin", "participant"]) }
 
   it "checks that a trip can only be associated with a user once" do
-    user = User.create!(
-      first_name: "François",
-      address: "Paris",
-      email: "francois@example.com",
-      password: "password",
-      phone: "3636")
-
-    trip = Trip.create!(name: "Voyage", start_date: Date.today, end_date: Date.tomorrow)
-
-    participation = Participation.create!(user: user, trip: trip, role: "admin")
-    participation2 = Participation.new(user: user, trip: trip, role: "participant")
+    Participation.create!(:user, :trip, role: "admin")
+    participation2 = Participation.new(:user, :trip, role: "participant")
 
     expect(participation2).to be_invalid
   end

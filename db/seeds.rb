13ca9f26a424dc13e6e387fbb356f3f89edcaa97
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-11-01 14:38:46 +0100

    Add trips and destinations to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@ User.destroy_all
 # Destroy all trips
 puts "Destroying Trips"
 Trips.destroy_all
+
+# Trips creation
+trip = Trip.new(name: "Ski 2025!", start_date: '2025-01-20', end_date: '2025-03-12')
+Destination.new(trip: trip, name: "La Clusaz")
+Destination.new(trip: trip, name: "Courchevel")
+Destination.new(trip: trip, name: "Meribel")
+Destination.new(trip: trip, name: "Puy-Saint-Vincent")
+
+trip = Trip.new(name: "Summer 25", start_date: '2025-06-26', end_date: '2025-08-31')
+Destination.new(trip: trip, name: "Mykonos")
+Destination.new(trip: trip, name: "Belle-Île-en-Mer")
+Destination.new(trip: trip, name: "Marrakech")
+Destination.new(trip: trip, name: "Madrid")
+Destination.new(trip: trip, name: "Barcelone")
+Destination.new(trip: trip, name: "Lisbonne")

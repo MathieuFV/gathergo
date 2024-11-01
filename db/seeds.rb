c64b22d7c4b69160ded5a2b28855b7ba9853b6d2
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-11-01 17:12:33 +0100

    Create users, trips, destinations and participations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,64 @@ User.destroy_all
 
 # Destroy all trips
 puts "Destroying Trips"
-Trips.destroy_all
+Trip.destroy_all
 
+# Users creation
+users = [
+  {
+    name: "Marin",
+    address: "Toulon"
+  },
+  {
+    name: "Mathieu",
+    address: "Paris"
+  },
+  {
+    name: "Pierre",
+    address: "Lyon"
+  }
+]
+users.each do |user|
+  puts "Creation de #{user[:name]}"
+  new_user = User.new(first_name: "#{user[:name]}", address: user[:address], email: "#{user[:name].downcase}@example.com", password: "password")
+
+  if new_user.save
+    photo_path = Rails.root.join("app/assets/images/users/#{user[:name].downcase}.png") # chemin vers votre photo
+    new_user.photo.attach(
+      io: File.open(photo_path),
+      filename: "#{user[:name].downcase}.png",
+      content_type: "image/png" # Assurez-vous que le type de contenu est correct
+    )
+  end
+end
+
+puts "Creation de Ski 2025"
 # Trips creation
-trip = Trip.new(name: "Ski 2025!", start_date: '2025-01-20', end_date: '2025-03-12')
-Destination.new(trip: trip, name: "La Clusaz")
-Destination.new(trip: trip, name: "Courchevel")
-Destination.new(trip: trip, name: "Meribel")
-Destination.new(trip: trip, name: "Puy-Saint-Vincent")
+trip = Trip.create(name: "Ski 2025!", start_date: '2025-01-20', end_date: '2025-03-12')
+puts "Ajout des destinations à Ski 2025"
+# Destinations for trip Ski 2025
+Destination.create(trip: trip, name: "La Clusaz")
+Destination.create(trip: trip, name: "Courchevel")
+Destination.create(trip: trip, name: "Meribel")
+Destination.create(trip: trip, name: "Puy-Saint-Vincent")
+# Participations for trip ski 2025
+puts "Ajout des participants à Ski 2025"
+Participation.create(user: User.where(first_name: "Marin").first, trip: trip, role: "admin" )
+Participation.create(user: User.where(first_name: "Mathieu").first, trip: trip, role: "participant" )
+Participation.create(user: User.where(first_name: "Pierre").first, trip: trip, role: "participant" )
 
-trip = Trip.new(name: "Summer 25", start_date: '2025-06-26', end_date: '2025-08-31')
-Destination.new(trip: trip, name: "Mykonos")
-Destination.new(trip: trip, name: "Belle-Île-en-Mer")
-Destination.new(trip: trip, name: "Marrakech")
-Destination.new(trip: trip, name: "Madrid")
-Destination.new(trip: trip, name: "Barcelone")
-Destination.new(trip: trip, name: "Lisbonne")
+puts "Creation de Summer 25"
+trip = Trip.create(name: "Summer 25", start_date: '2025-06-26', end_date: '2025-08-31')
+# Destinations for trip Summer 25
+puts "Ajout des destinations à Summer 25"
+Destination.create(trip: trip, name: "Mykonos")
+Destination.create(trip: trip, name: "Belle-Île-en-Mer")
+Destination.create(trip: trip, name: "Marrakech")
+Destination.create(trip: trip, name: "Madrid")
+Destination.create(trip: trip, name: "Barcelone")
+Destination.create(trip: trip, name: "Lisbonne")
+# Participations
+puts "Ajout des participants à Summer 25"
+Participation.create(user: User.where(first_name: "Mathieu").first, trip: trip, role: "admin" )
+Participation.create(user: User.where(first_name: "Marin").first, trip: trip, role: "participant" )
+Participation.create(user: User.where(first_name: "Pierre").first, trip: trip, role: "participant" )

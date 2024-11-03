642cdb86606d5abc84a190ab754540ba046c36d5
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-11-03 13:09:02 +0100

    Redirect add button to new form for adding a destination

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -9,4 +9,28 @@ class DestinationsController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @destination = Destination.find(params[:id])
   end
+
+  def new
+    @trip = Trip.find(params[:trip_id])
+    @destination = Destination.new
+  end
+
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @destination = Destination.new(destination_params)
+
+    @destination.trip = @trip
+
+    if @destination.save
+      redirect_to trip_path(@trip)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:name)
+  end
 end

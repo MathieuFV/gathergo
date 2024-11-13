450ed5ad26ecd948fc613e4c1cc6ccbaa46de319
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-11-13 15:16:02 +0100

    Apply zooming on all the carousel pictures when clicking on it

--- app/javascript/controllers/zoom_controller.js
+++ app/javascript/controllers/zoom_controller.js
@@ -2,11 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="zoom"
 export default class extends Controller {
+  static targets = ["photo"]
+
   connect() {
     console.log("Zoom controller")
   }
 
-  zoom = (event) => {
-    event.target.classList.toggle("zoomed")
+  zoom = () => {
+    // event.target.classList.toggle("zoomed")
+    console.log(this.photoTargets)
+    this.photoTargets.forEach(photo => {
+      photo.classList.toggle("zoomed")
+    })
   }
 }

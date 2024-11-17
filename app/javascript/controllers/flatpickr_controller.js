b9b901763b590f3787e4845166797e301e9b262a
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-11-17 20:41:58 +0100

    add Flatpickr to the datepicker page

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
+    console.log("Connected to flatpickr stimulus", this.element)
+
+    flatpickr(this.element.querySelector(".start-date"), {
+      dateFormat: "F j, Y",
+      mode: "range"
+    });
+    flatpickr(this.element.querySelector(".end-date"), {
+      dateFormat: "F j, Y"
+    });
   }
 }

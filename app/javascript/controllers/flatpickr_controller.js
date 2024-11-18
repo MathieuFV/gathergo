7c31b21d14764717153cf7905e2900fb87c03b78
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-11-18 21:33:58 +0100

    Add datepicker calendar to pick up range availabilities

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,15 +3,27 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
+  static targets = ["startDate", "endDate"]
+
   connect() {
     console.log("Connected to flatpickr stimulus", this.element)
 
+    this.startDateTarget.value
+
     flatpickr(this.element.querySelector(".start-date"), {
       dateFormat: "F j, Y",
-      mode: "range"
-    });
-    flatpickr(this.element.querySelector(".end-date"), {
-      dateFormat: "F j, Y"
+      mode: "range",
+      closeOnSelect: false,
+      onClose: (selectedDates) => {
+        this.startDateTarget.value = selectedDates[0]
+        this.endDateTarget.value = selectedDates[1]
+        console.log("end date:" + this.endDateTarget.value)
+      }
     });
   }
 }
+
+// clickOpens: false  //Whether clicking on the input should open the picker. You could disable this if you wish to open the calendar manually with.open()
+
+// Récupération des deux dates
+//
